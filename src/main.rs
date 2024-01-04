263df1f718585eab889188977b3b958b0f4902d4
HactarCE <6060305+HactarCE@users.noreply.github.com>
2024-01-04 01:30:21 -0500

    Add crate description

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+//! Opinionated MIDI looper.
+
 #![warn(
     rust_2018_idioms,
-    // missing_docs,
+    missing_docs,
     clippy::if_then_some_else_none,
     clippy::manual_let_else,
     clippy::semicolon_if_nothing_returned,
